708b3d8dc2ffc11a15d44581c166ab15847b5d8f
Jose Mendoza <pball626@gmail.com>
2021-02-05 17:28:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,10 +7,10 @@ def introduction(Jose)
 end
 
 def introduction_with_language(Jose, Ruby)
-puts "Hi, my name is #{name} and I am learning to program in #{Ruby}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language = "Ruby")
+def introduction_with_language_optional(Jose, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{Ruby}."
 end
 
